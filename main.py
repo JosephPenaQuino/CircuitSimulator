1dd2e4cecb071eb38c4c9671fd12cc1f5a366915
Joseph <joseph.pena@utec.edu.pe>
2017-07-30 14:34:45 -0500

    finished

--- main.py
+++ main.py
@@ -7,33 +7,46 @@ class Component(object):
     def __init__(self, name, **kwargs):
         self.name = name
         self.kwarg = kwargs
-        self.active = False
+        self.active = False  # Set if a component is active or not
         for name, value in kwargs.items():
-            try:
-                node[value].append(self)
-            except:
-                node.append([])
-                node[value].append(self)
+            if name == 'p0' or name == 'p1' or name == 'p2':
+                try:
+                    node[value].append(self)
+                except IndexError:
+                    node.append([])
+                    node[value].append(self)
 
 
 class Source(Component):
-    def __init__(self):
-        Component.__init__(self)
-
+    def __init__(self, name, **kwargs):
+        Component.__init__(self, name, **kwargs)
         self.active = True
+        try:
+            self.voltage = kwargs['voltage']
+        except:
+            try:
+                self.current = kwargs['current']
+            except:
+                self.voltage = 12
 
-# class Resistor(Component):
-#     def __init__(self):
-#         Component.__init__(self)
-#         try:
-#             self.ohm = self.kwarg['Ohm']
-#         except:
-#             self.ohm = 0
-#             print 'No Ohms?'
 
-r1 = Component('r1', p0=0, p1=1)
-r2 = Component('r2', p0=1, p1=2)
-r3 = Component('r3', p0=1, p1=2)
-r4 = Component('r4', p0=2, p1=3)
-r5 = Component('r5', p0=3, p1=0)
-k = Source('s0',p0=3,p1=0)
+class Resistor(Component):
+    def __init__(self, name, **kwargs):
+        Component.__init__(self, name, **kwargs)
+        try:
+            self.resistance = kwargs['resistance']
+        except:
+            self.resistance = 2200
+
+
+r1 = Resistor('r1', p0=0, p1=1, resistance=1000)
+r2 = Resistor('r2', p0=1, p1=2, resistance=1200)
+r3 = Resistor('r3', p0=1, p1=2, resistance=4000)
+r4 = Resistor('r4', p0=2, p1=3, resistance=5010)
+r5 = Resistor('r5', p0=3, p1=0)
+k = Source('s0', p0=3, p1=0)
+
+for i in node:
+    for k in i:
+        print k.name
+    print '------'
